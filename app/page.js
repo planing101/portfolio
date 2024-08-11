cd8f0a73470bbaab543cd21f7be6a71f65939266
Peter Laning <peter_laning@hotmail.com>
2024-08-12 08:53:06 +1000

    Add tech image.

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className={styles.banner}>
-      <div className={styles.slider} style={{"--quantity": 7}}>
+      <div className={styles.slider} style={{"--quantity": 8}}>
         <div className={styles.item} style={{"--position": 1}}><img src="images/dot-net_200x200.svg"/></div>
         <div className={styles.item} style={{"--position": 2}}><img src="images/csharp_200x200.svg"/></div>
         <div className={styles.item} style={{"--position": 3}}><img src="images/javascript_200x200.svg"/></div>
@@ -15,6 +15,7 @@ export default function Home() {
         <div className={styles.item} style={{"--position": 5}}><img src="images/react_200x200.svg"/></div>
         <div className={styles.item} style={{"--position": 6}}><img src="images/css_200x200.svg"/></div>
         <div className={styles.item} style={{"--position": 7}}><img src="images/ux-ui_200x200.svg"/></div>
+        <div className={styles.item} style={{"--position": 8}}><img src="images/azure-devops_200x200"/></div>
       </div>
       <div className={styles.content}>
         <h1 className={inter.className}>Portfolio</h1>
