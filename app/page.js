9c4a89cf9399e22eed861b80ed0d24ef8f615354
Peter Laning <peter_laning@hotmail.com>
2024-08-12 12:49:02 +1000

    Update tech images.

--- app/page.js
+++ app/page.js
@@ -8,14 +8,14 @@ export default function Home() {
   return (
     <div className={styles.banner}>
       <div className={styles.slider} style={{"--quantity": 8}}>
-        <div className={styles.item} style={{"--position": 1}}><img src="images/dot-net_200x200.svg"/></div>
-        <div className={styles.item} style={{"--position": 2}}><img src="images/csharp_200x200.svg"/></div>
-        <div className={styles.item} style={{"--position": 3}}><img src="images/javascript_200x200.svg"/></div>
-        <div className={styles.item} style={{"--position": 4}}><img src="images/sql-server_200x200.svg"/></div>
-        <div className={styles.item} style={{"--position": 5}}><img src="images/react_200x200.svg"/></div>
-        <div className={styles.item} style={{"--position": 6}}><img src="images/css_200x200.svg"/></div>
-        <div className={styles.item} style={{"--position": 7}}><img src="images/ux-ui_200x200.svg"/></div>
-        <div className={styles.item} style={{"--position": 8}}><img src="images/azure-devops_200x200.svg"/></div>
+        <div className={styles.item} style={{"--position": 1}}><img src="portfolio/images/dot-net_200x200.svg"/></div>
+        <div className={styles.item} style={{"--position": 2}}><img src="portfolio/images/csharp_200x200.svg"/></div>
+        <div className={styles.item} style={{"--position": 3}}><img src="portfolio/images/javascript_200x200.svg"/></div>
+        <div className={styles.item} style={{"--position": 4}}><img src="portfolio/images/sql-server_200x200.svg"/></div>
+        <div className={styles.item} style={{"--position": 5}}><img src="portfolio/images/react_200x200.svg"/></div>
+        <div className={styles.item} style={{"--position": 6}}><img src="portfolio/images/css_200x200.svg"/></div>
+        <div className={styles.item} style={{"--position": 7}}><img src="portfolio/images/ux-ui_200x200.svg"/></div>
+        <div className={styles.item} style={{"--position": 8}}><img src="portfolio/images/azure-devops_200x200.svg"/></div>
       </div>
       <div className={styles.content}>
         <h1 className={inter.className}>Portfolio</h1>
