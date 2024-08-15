793a08e564c4635ca73d2a0de4a2a2dcc6d60a0c
Peter Laning <peter_laning@hotmail.com>
2024-08-15 18:16:31 +1000

    Implement modal.

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import Image from 'next/image';
 import Link from 'next/link';
 import Modal from './components/modal';
+import Dialog from './components/dialog';
 import React, { useState } from 'react';
 
 import styles from "./page.module.css";
@@ -27,15 +28,16 @@ export default function Home() {
   return (
     <>
       <div className={styles.banner}>
-        <div className={styles.slider} style={{"--quantity": 8}}>
+        <div className={styles.slider} style={{"--quantity": 9}}>
           <div className={styles.item} style={{"--position": 1}} onClick={openModal} title="Click me"><img src={`${baseRoute}/images/dot-net_200x200.svg`}/></div>
           <div className={styles.item} style={{"--position": 2}}><img src={`${baseRoute}/images/csharp_200x200.svg`}/></div>
           <div className={styles.item} style={{"--position": 3}}><img src={`${baseRoute}/images/javascript_200x200.svg`}/></div>
           <div className={styles.item} style={{"--position": 4}}><img src={`${baseRoute}/images/sql-server_200x200.svg`}/></div>
           <div className={styles.item} style={{"--position": 5}}><img src={`${baseRoute}/images/react_200x200.svg`}/></div>
-          <div className={styles.item} style={{"--position": 6}}><img src={`${baseRoute}/images/css_200x200.svg`}/></div>
-          <div className={styles.item} style={{"--position": 7}}><img src={`${baseRoute}/images/ux-ui_200x200.svg`}/></div>
-          <div className={styles.item} style={{"--position": 8}}><Link href="/new"><Image src={`${baseRoute}/images/azure-devops_200x200.svg`} width={200} height={200} alt="Azure DevOps"/></Link></div>
+          <div className={styles.item} style={{"--position": 6}}><img src={`${baseRoute}/images/angularjs_200x200.svg`}/></div>
+          <div className={styles.item} style={{"--position": 7}}><img src={`${baseRoute}/images/css_200x200.svg`}/></div>
+          <div className={styles.item} style={{"--position": 8}}><img src={`${baseRoute}/images/ux-ui_200x200.svg`}/></div>
+          <div className={styles.item} style={{"--position": 9}}><Link href="/new"><Image src={`${baseRoute}/images/azure-devops_200x200.svg`} width={200} height={200} alt="Azure DevOps"/></Link></div>
         </div>
         <div className={styles.content}>
           <h1 className={inter.className}>Portfolio</h1>
@@ -49,6 +51,9 @@ export default function Home() {
       <Modal title="Modal Title" isOpen={isModalOpen} onClose={closeModal}>
         <p>This is the modal content</p>
       </Modal>
+      {/* <Dialog title="Dialog Title" isOpen={isModalOpen} onClose={closeModal}>
+        <p>This is the dialog content</p>
+      </Dialog> */}
     </>
   );
 }
