bf36e2ce90d2c0d67831a6784a3e36ef271c09c1
Peter Laning <peter_laning@hotmail.com>
2024-09-18 17:53:58 +1000

    Add container to hand component.

--- app/components/helpingHand.js
+++ app/components/helpingHand.js
@@ -1,45 +1,9 @@
-import Image from "next/image";
-// import { useEffect, useRef } from "react";
-// import { transform } from "next/dist/build/swc";
-
 import styles from "./helpingHand.module.css"
 
 export default function HelpingHand({ children }) {
-    //const movingBoxRef = useRef(null);
-    //const targetBoxRef = useRef(null);
-
-    // useEffect(() => {
-    //     const movingBox = movingBoxRef.current;
-    //     const targetBox = document.getElementById('Slider');
-
-    //     const moveToTarget = () => {
-    //         if (targetBox && movingBox) {
-    //             const targetRect = targetBox.getBoundingClientRect();
-    //             const boxRect = movingBox.getBoundingClientRect();
-                
-    //             // Calculate the translation values
-    //             const translateX = targetRect.left - boxRect.left;
-    //             const translateY = targetRect.top - boxRect.top;
-
-    //             // Apply the translation
-    //             movingBox.style.transform = `translate(${translateX}px, ${translateY}px)`;
-    //         }
-    //     };
-
-    //     // Move the box every second
-    //     //const intervalId = window.setInterval(moveToTarget, 1000);
-
-    //     // Cleanup the interval on component unmount
-    //     //return () => clearInterval(intervalId);
-    // }, []);
-    
     return (
-        <>
-        {/* <div ref={movingBoxRef} className={styles.hintHand}> */}
-        <div className={styles.hintHand}>
-            {/* <Image src="/portfolio/images/pointer-1_200x200.png" width={200} height={200} alt="Helping hand" unoptimized/> */}
+        <div className={styles.hintHandContainer}>
+            <div className={styles.hintHand}></div>
         </div>
-        
-        </>
     );
 };
